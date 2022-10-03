6a30dfbaf7def02ee231c70986937c3e26b9a254
Hrayr Dilbaryan <hrair.dilbaryan@mail.ru>
2022-10-03 18:30:07 +0400

    Health Center For Soldiers

---
+++ src/main/java/com/example/healthcenterforsoldiers/HealthCenterForSoldiersApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcenterforsoldiers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCenterForSoldiersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCenterForSoldiersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/healthcenterforsoldiers/HealthCenterForSoldiersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcenterforsoldiers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCenterForSoldiersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
