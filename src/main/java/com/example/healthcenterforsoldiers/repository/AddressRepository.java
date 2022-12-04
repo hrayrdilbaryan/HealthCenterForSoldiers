01420ce0e193df5b455d1e799e984b44546b8447
Hrayr Dilbaryan <hrair.dilbaryan@mail.ru>
2022-12-04 16:12:38 +0400

    init repo ...

---
+++ src/main/java/com/example/healthcenterforsoldiers/repository/AddressRepository.java
@@ -0,0 +1,6 @@
+package com.example.healthcenterforsoldiers.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Addess, Integer> {
+}
